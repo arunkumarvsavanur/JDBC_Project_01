e87c3956a0b601e7ec4882cf234029e255c9ffcc
arunkumarvsavanur <132760411+arunkumarvsavanur@users.noreply.github.com>
2025-05-06 09:35:47 +0530

    Update Program2.java

--- src/Package/Program2.java
+++ src/Package/Program2.java
@@ -1,11 +1,12 @@
 package Package;
-
+import java.util.Scanner;
 public class Program2 {
 
 	public static void main(String [] args) {
 		System.out.println("Hello world!!!");
 		Scanner sc=new Scanner(System.in);
-		System,out,println("Enter a number:");
+		//Square of a number
+		System,out.println("Enter a number:");
 		int n =s.nextInt();
 		System.out,println("Square of"+n+"is:"+(n*n));
 	}
