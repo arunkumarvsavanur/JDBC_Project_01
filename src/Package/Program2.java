2e06c54394caee1fd220f35a6df85e389b90a6e0
arunkumarvsavanur <132760411+arunkumarvsavanur@users.noreply.github.com>
2025-05-06 09:28:47 +0530

    Update Program2.java

--- src/Package/Program2.java
+++ src/Package/Program2.java
@@ -3,6 +3,10 @@ package Package;
 public class Program2 {
 
 	public static void main(String [] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world!");
+		Scanner sc=new Scanner(System.in);
+		System,out,println("Enter a number:");
+		int n =s.nextInt();
+		System.out,println("Square of"+n+"is:"+(n*n));
 	}
 }
